5c71fbc9f1a56aa172a9fe507f566b13abd98dcf
Aaron Korte <aaron.korte@atmina.de>
2024-10-07 11:42:25 +0200

    Add dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -94,4 +94,31 @@ return {
       }
     end,
   },
+  -- Thanks Heise! https://www.heise.de/ratgeber/Fuer-Programmierer-Die-Entwicklungsumgebung-Neovim-anpassen-erweitern-9869214.html?seite=3
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+          },
+        },
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
 }
